247d46bbf68693d9994485ef5ee6826eb425b019
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-02-25 15:01:04 +0100

    snacks

---
+++ js/script.js
@@ -0,0 +1,66 @@
+//QUESTO è LO SNACK NUMERO 1
+
+// const num1 = parseInt(prompt('NUmero 1:'));
+// const num2 = parseInt(prompt('NUmero 2:'));
+
+// if (num1 > num2) {
+//     console.log(num1);
+// } else if (num1 < num2) {
+//     console.log(num2);
+// } else {
+//     console.log('i numeri sono uguali');
+// }
+
+
+
+
+//QUESTO è LO SNACK NUMERO 2
+
+// let parolaNumero1 = prompt('scrivi una parola');
+// let parolaNumero2 = prompt('scrivi la seconda parola');
+
+
+// if (parolaNumero1.length > parolaNumero2.length) {
+//     document.writeln(parolaNumero2);
+//     document.writeln(`<br>`);
+//     document.writeln(parolaNumero1);
+
+// } else if (parolaNumero1.length < parolaNumero2.length) {
+
+//     document.writeln(parolaNumero1);
+//     document.writeln(`<br>`);
+//     document.writeln(parolaNumero2);
+
+// } else {
+
+//     console.log('le parole hanno la stessa lunghezza');
+
+// }
+
+
+
+
+//QUESTO è LO SNACK NUMERO 3
+// let arrNum = [];
+
+// for (let i = 0; i < 10; i++) {
+
+//     let userNumber = parseInt(prompt('scrivi un numero'))
+//     arrNum.push(userNumber);
+// }
+
+// console.log(arrNum);
+
+
+// let somma = 0;
+// for (let i = 0; i < arrNum.length; i++) {
+//     somma = somma + arrNum[i];
+// }
+
+// console.log(somma);
+
+
+
+
+
+//QUESTO è LO SNACK NUMERO 4
